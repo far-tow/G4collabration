e2e0742d2698e01d62ecef2684407cd04bdc3089
NiveSJ <niveathithavj@gmail.com>
2022-10-26 13:32:43 +0200

    Added class file.

---
+++ src/main/java/se/lexicon/NameRepository.java
@@ -0,0 +1,42 @@
+package se.lexicon;
+
+
+
+  /**
+   public static void clear(), public static String[] findByFirstName(final String firstName)   for farhad
+
+
+   public static String[] findAll(), public static boolean add(final String fullName) ; lilly
+
+
+   -findByLastName,  boolean remove(final String fullName) ; sam
+
+
+    find full name,    update; Nivethitha
+
+**/
+   public class NameRepository {
+
+
+
+    private static String[] names;
+
+
+    public static int getSize(){
+        return 0;
+    }
+    public static void setNames(String[] names)
+    {}
+
+    public static void clear(){}
+
+    public static String[] findAll(){
+
+        return new String[0];
+    }
+
+
+
+
+
+}
