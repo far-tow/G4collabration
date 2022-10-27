97c8a2b1346de28b708bc1c9c4ce8028450ce5c4
liljana ristevska <li.ristevska@gmail.com>
2022-10-27 15:24:17 +0200

    my part

--- src/main/java/se/lexicon/NameRepository.java
+++ src/main/java/se/lexicon/NameRepository.java
@@ -23,122 +23,35 @@ public class NameRepository {
     SUGGESTIONS:  */
     private static String names[] = new String[0];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public static String[] findAll() {
-        String[] names = {"Samuel Svahn", "Nivethitha Jothikumar", "Liljana Ristevska", "Farhad Towfighian", "Veronica Okoli"};
-        // Arrays.sort(names, String.CASE_INSENSITIVE_ORDER);
-    /* Veronica
-    public static int getSize()
-    Returns number of elements in the array
-    SUGGESTIONS: */
     public static int getSize() {
 
     } //Should have a Return!
 
-        // System.out.println(Arrays.toString(names));
-        String[] result = Arrays.copyOf(names, names.length);
-
-    /* Who?
-    public static void setNames(String[] names)
-    Sends in an array that set the private static array.
-    This should replace all existing names.
-    SUGGESTIONS:  */
     public static void setNames(String[] names) {
 
     }
-
-        return result;
-    /*Farhad
-    public static void clear()
-    Should completely empty the array.
-    SUGGESTIONS: V:String[0] will works! S: yes it works   */
 
     public static void clear() {
         names = new String[0];
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /* Lilly
-    public static String[] findAll()
-    Returns all names in a new array
-    SUGGESTIONS:  */
     public static String[] findAll() {
+        String[] names = {"Samuel Svahn", "Nivethitha Jothikumar", "Liljana Ristevska", "Farhad Towfighian", "Veronica Okoli"};
+        // Arrays.sort(names, String.CASE_INSENSITIVE_ORDER);
+        // System.out.println(Arrays.toString(names));
+        String[] result = Arrays.copyOf(names, names.length);
 
-    } //Should have a Return!
+        return result;
 
-    /* Nivethitha
-    public static String find(final String fullName)
-    Returns name if found and null if not found.
-    SUGGESTIONS:  */
-    public static String find(final String fullName) {
-        for (String na : names){
-            if (fullName.equalsIgnoreCase(na))
-                return na;}
-        return "Name not found";
 
-    } //Should have a Return!
+        public static String find ( final String fullName){
+            for (String na : names) {
+                if (fullName.equalsIgnoreCase(na))
+                    return na;
+            }
+            return "Name not found";
+
+        }
 
     /* Lilly
     public static boolean add(final String fullName)
@@ -147,132 +60,64 @@ public class NameRepository {
     SUGGESTIONS:  */
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public static boolean add(final String fullName) {
-        String[] names;
-        String newArray[] = Arrays.copyOf(names, names.length + 1);
-        names = new String[];
-        // addNameToArray(names," ");
-        boolean flag = true;
-        for (String name : names) {
-            if (name.equals(fullName)) {
-                flag = false;
-                System.out.println("Name found cannot add");
-            }
-
-    }
-        public static String[] findByLastName (final String LastName){
-
-
-        }
-        if (flag) {
+        public static boolean add ( final String fullName){
+            String[] names;
             String newArray[] = Arrays.copyOf(names, names.length + 1);
-            newArray[newArray.length - 1] = fullName;
-            System.out.println(Arrays.toString(newArray));
-
-
-            boolean ifNameExists = false;
+            names = new String[];
+            // addNameToArray(names," ");
+            boolean flag = true;
             for (String name : names) {
-                if (!name.equals(fullName)) ;
-                // ifNameExists = true;
+                if (name.equals(fullName)) {
+                    flag = false;
+                    System.out.println("Name found cannot add");
+                }
+
             }
-            if (ifNameExists == true) {
-                return false;
-            } else {
-                //newArray[names.length] = fullName;
-                return true;
+            public static String[] findByLastName ( final String LastName){
+
+
+            }
+            if (flag) {
+                String newArray[] = Arrays.copyOf(names, names.length + 1);
+                newArray[newArray.length - 1] = fullName;
+                System.out.println(Arrays.toString(newArray));
+
+
+                boolean ifNameExists = false;
+                for (String name : names) {
+                    if (!name.equals(fullName)) ;
+                    // ifNameExists = true;
+                }
+                if (ifNameExists == true) {
+                    return false;
+                } else {
+                    //newArray[names.length] = fullName;
+                    return true;
+                }
+
             }
 
-        }
-
-    } //Should have a Return!
+        } //Should have a Return!
 
     /* Farhad
     public static String[] findByFirstName(final String firstName)
     Searches the array trying to find all names that has passed in first name.
     Returns a String array containing all matches.
     SUGGESTIONS:  */
-    public static String[] findByFirstname(final String firstName) {
-        String splitNames[];
-        for (String na : names) {
-            splitNames = na.split(" ");
-            if (splitNames.length >= 2 && splitNames[0].equalsIgnoreCase(firstname)) {
-                System.out.println("First name is: " + firstname);
-                return "Full name is: " + na;
+        public static String[] findByFirstname ( final String firstName){
+            String splitNames[];
+            for (String na : names) {
+                splitNames = na.split(" ");
+                if (splitNames.length >= 2 && splitNames[0].equalsIgnoreCase(firstname)) {
+                    System.out.println("First name is: " + firstname);
+                    return "Full name is: " + na;
+                }
             }
-        }
-        return "NAME NOT FOUND";
+            return "NAME NOT FOUND";
 
-    } //Should have a Return!
-
-
+        } //Should have a Return!
 
     }
-
 
     /* Samuel
     public static boolean remove(final String fullName)
